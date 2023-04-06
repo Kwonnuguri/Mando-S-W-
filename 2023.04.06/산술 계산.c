50c6c6c59e0f47fad0e779795ab5eca5e2a8e77b
Kwonnuguri <129155856+Kwonnuguri@users.noreply.github.com>
2023-04-06 18:52:25 +0900

    Add files via upload

---
+++ 2023.04.06/산술 계산.c
@@ -0,0 +1,27 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+int main()
+{
+	char op;
+	int x, y, result;
+
+	printf("수식을 입력하시오(예: 2 + 5) >> ");
+	scanf("%d %c %d", &x, &op, &y);
+
+	if (op == '+')
+		result = x + y;
+	else if (op == '-')
+		result = x - y;
+	else if (op == '*')
+		result = x * y;
+	else if (op == '/')
+		result = x / y;
+	else if (op == '%')
+		result = x % y;
+	else
+		printf("지원되지 않는 연산자입니다. \n");
+
+	printf("%d %c %d = %d \n", x, op, y, result);
+	return 0;
+
+}
