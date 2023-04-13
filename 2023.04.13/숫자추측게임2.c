3a823d413bea26aa2349dc7b28ebc8b4c4362398
Kwonnuguri <129155856+Kwonnuguri@users.noreply.github.com>
2023-04-13 19:26:16 +0900

    Add files via upload

---
+++ 2023.04.13/숫자추측게임2.c
@@ -0,0 +1,28 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main()
+{
+	srand((unsigned)time(NULL));
+	int answer = rand() % 100; //정답을 난수로 발생한다.
+	int guess;
+	int tries = 0;
+	// 반복 구조
+	do {
+		printf("정답을 추측하여 보시오: ");
+		scanf("%d", &guess);
+		tries++;
+
+		if (guess > answer) //사용자가 입력한 정수가 정답보다 높으면
+			printf("숫자가 큽니다 \n");
+		if (guess < answer) //사용자가 입력한 정수가 정답보다 낮으면
+			printf("숫자가 작습니다 \n");
+	} while (guess != answer);
+
+	printf("축하합니다. 시도횟수=%d\n", tries);
+	return 0;
+
+
+}
---
+++ 2023.04.13/주사위 도전문제.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+
+	for (int a = 1; a <= 6; a++)
+	{
+		for (int b = 1; b <= 6; b++)
+		{
+			for (int c = 1; c <= 6; c++)
+			{
+				if (a + b + c == 10)
+					printf("%d, %d\n", a, b, c);
+			}
+		}
+	}
+}
